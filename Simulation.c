83d6ffb01ac19ee6117db7d4e54d848f122c1138
JulianoF <juliano1@live.ca>
2024-03-27 21:43:03 -0400

    Working Sim, Needs cleanup and testing

--- Simulation.c
+++ Simulation.c
@@ -1,13 +1,62 @@
+#include <time.h> 
+#include <unistd.h>
 
 #include "core/Banker.h"
 
 pthread_mutex_t mutex;
 
 void *customer_thread(void *arg){
+    int customer_num = *(int *)arg;
+    int rand_request[NUMBER_OF_RESOURCES];
+    int rand_release[NUMBER_OF_RESOURCES];
+    while(1){
+        sleep(rand() % 5); //FOR TESTING
+        printf("\n****************************************************\n");
+        pthread_mutex_lock(&mutex);
 
+        //generate random request
+        for(int i = 0; i < NUMBER_OF_RESOURCES; i++){
+            rand_request[i] = rand() % (maximum[customer_num][i] + 1);
+        }
+        printf("Customer %d is Requesting: %d, %d, %d resources\n",customer_num,rand_request[0],rand_request[1],rand_request[2]);
+        int req = request_resources(customer_num,rand_request);
+        if(req == -1){
+            printf("Request Failed\n");
+            //pthread_mutex_unlock(&mutex); 
+            //continue;
+            //goto Release;
+        }
+        
+        if(!is_safe_state()){
+            printf("Restoring State\n");
+            release_resources(customer_num,rand_request);
+            pthread_mutex_unlock(&mutex); 
+            continue;
+        }  
+        printf("Customer %d Using %d %d %d resources\n",customer_num,allocation[customer_num][0],allocation[customer_num][1],allocation[customer_num][2]);
+    //Release:
+        //generate random release
+        for(int i = 0; i < NUMBER_OF_RESOURCES; i++){
+            rand_release[i] = rand() % (allocation[customer_num][i] + 1);
+        }
+
+        printf("Customer %d is Releasing: %d, %d, %d resources\n",customer_num,rand_release[0],rand_release[1],rand_release[2]);
+        int rel = release_resources(customer_num,rand_release);
+        if(rel == -1){
+            printf("Release Failed\n");
+        }
+        pthread_mutex_unlock(&mutex); 
+        //sleep(rand() % 5); //FOR TESTING
+    }
+    pthread_exit(NULL);
 }
 
 int main(int argc, char** argv){
+
+    pthread_t threads[NUMBER_OF_CUSTOMERS];
+    int thread_args[NUMBER_OF_CUSTOMERS];
+    srand(time(0)); 
+
     if(argc < 4 || argc > 4){
         printf("Must have 3 args to represent resources to allocate\n");
         exit(1);
@@ -26,4 +75,20 @@ int main(int argc, char** argv){
 
     pthread_mutex_init(&mutex, NULL);
 
+    for (int i = 0; i < NUMBER_OF_CUSTOMERS; i++) {
+        thread_args[i] = i; 
+        if (pthread_create(&threads[i], NULL, customer_thread, &thread_args[i]) != 0) {
+            printf("Error creating thread: %d\n", i);
+            exit(1);
+        }
+    }
+
+    for (int i = 0; i < NUMBER_OF_CUSTOMERS; i++) {
+        if (pthread_join(threads[i], NULL) != 0) {
+            printf("Error joining thread: %d\n", i);
+            exit(1);
+        }
+    }
+    pthread_mutex_destroy(&mutex);
+    return 0;
 }
--- core/Banker.c
+++ core/Banker.c
@@ -22,16 +22,32 @@ int request_resources(int customer_num, int request[]){
                 allocation[customer_num][i] = allocation[customer_num][i] + request[i];
                 need[customer_num][i] = need[customer_num][i] - request[i];
             }else{
+                printf("Resource Request for Customer %d can not be fulfilled\n",customer_num);
                 return -1;
             }
         }else{
+            printf("Error, Customer %d is requesting more resources than it needs\n",customer_num);
             return -1;
         }
     }
+    printf("Resource Request for Customer %d can be fulfilled sucessfully\n",customer_num);
     return 0;
 }
 
 int release_resources(int customer_num, int release[]){
+    for (int i = 0; i < NUMBER_OF_RESOURCES; i++) {
+        if (release[i] > allocation[customer_num][i]) {
+            printf("Error, Customer %d is releasing more resources than it has allocated\n", customer_num);
+            return -1;
+        }
+    }
+    for (int i = 0; i < NUMBER_OF_RESOURCES; i++) {
+        available[i] += release[i];
+        allocation[customer_num][i] -= release[i];
+        need[customer_num][i] += release[i];  
+    }
+
+    printf("Resource release for Customer %d fulfilled successfully\n", customer_num);
     return 0;
 }
 
@@ -45,7 +61,7 @@ bool is_safe_state(){
     }
     for(int i = 0;i< NUMBER_OF_CUSTOMERS; i++){
         for(int j = 0;j < NUMBER_OF_RESOURCES; j++){
-            if(finished[j]==false && need[i][j] <= work[j]){
+            if(finished[i]==false && need[i][j] <= work[j]){
                 work[j] = work[j] + allocation[i][j];
                 finished[i] = true;
             }
@@ -60,6 +76,6 @@ bool is_safe_state(){
             break;
         }
     }
-
+    printf("Current state is safe?: %s\n", safe ? "True" : "False");
     return safe;
 }
